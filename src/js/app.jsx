68b27686bbc510881625111fcf3d4e0e0afa1740
Rovian Vieceli <rovian.vieceli@cwi.com.br>
2019-10-16 13:31:32 -0300

    hello world

---
+++ src/js/app.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const App = (
+    <div className="container">
+        <h1>Hello React</h1>
+    </div>
+)
+
+ReactDOM.render(App, document.getElementById('app'));

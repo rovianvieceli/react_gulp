84cdbd0f02a293fc466cdf9814435213b03b7cbf
Rovian Vieceli <rovian.vieceli@cwi.com.br>
2019-10-17 15:46:10 -0300

    Ciclo de vida

--- src/components/header/title.jsx
+++ src/components/header/title.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 
 class Title extends React.Component {
+    /**
+     * Ínicio do ciclos de vida
+     *  - antes do componente existir
+     */
+    constructor(props) {
+        super(props)
+        this.state = { hora: new Date() }
+    }
+
+    /**
+     * Quando componente é inicializado em tela
+     *  - componente "vivo"
+     */
+    componentDidMount() {
+        this.horaId = setInterval(() => (
+            this.setState({ hora: new Date() }),
+        1000))
+    }
+
+    /**
+     * Fim do ciclo de vida do componente
+     *  - componente é destruido
+     */
+    componentWillMount() {
+        clearInterval(this.horaId)
+    }
+
     render() {
-        return <h1>Olá eu sou um Component</h1>;
+        const { hora } = this.state;
+
+        return <h1>{`Olá eu sou um Component ${hora.toLocaleTimeString()}`}</h1>;
     }
 }
 

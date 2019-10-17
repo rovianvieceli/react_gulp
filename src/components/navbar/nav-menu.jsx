8b02fbfa0e6e7d9f85795c4b2f3b208e3f832b76
Rovian Vieceli <rovian.vieceli@cwi.com.br>
2019-10-17 15:34:04 -0300

    Aplicando estado

--- src/components/navbar/nav-menu.jsx
+++ src/components/navbar/nav-menu.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
 class NavMenu extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { active: 'Home' }
+        this.setActive = this.setActive.bind(this)
+    }
+
+    setActive(menu, event) {
+        this.setState({ active: menu.title })
+    }
+
     render() {
+        const self = this;
         const { menus } = this.props;
+        const { active } = this.state;
         return (
             <ul id="nav-mobile" className="right">
                 {menus.map((menu, index) => (
-                    <li key={index}>
-                        <a href={menu.linkTo}>{menu.title}</a>
+                    <li key={index} className={active == menu.title ? 'active' : ''}>
+                        <a href={menu.linkTo} onClick={this.setActive.bind(null, menu, this)}>{menu.title}</a>
                     </li>
                 ))}
             </ul>

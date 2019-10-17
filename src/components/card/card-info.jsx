a76cfa22be2b46419ec4824fdc471ae9ad03cd98
Rovian Vieceli <rovian.vieceli@cwi.com.br>
2019-10-17 17:07:56 -0300

    Herdando funcao do pai

--- src/components/card/card-info.jsx
+++ src/components/card/card-info.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
 class CardInfo extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handlerClick = this.handlerClick.bind(this)
+    }
+
+    handlerClick() {
+        this.props.fnClick()
+    }
+
     render() {
         const { data } = this.props;
         return (
             <div className="card sticky-action">
                 <div className="card-image waves-effect waves-block waves-light">
-                    <img className="activator" src={data.image} />
+                    <img className="activator" src={data.image} onClick={this.handlerClick} />
                 </div>
                 <div className="card-content">
                     <span className="card-title activator grey-text text-darken-4">{data.title}
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -3,8 +3,21 @@ import React from 'react';
 import CardInfo from './card-info';
 
 class Card extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { click: 0 }
+        this.setClick = this.setClick.bind(this)
+    }
+
+    setClick() {
+        this.setState((oldState) => ({
+            click: oldState.click + 1
+        }));
+    }
+
     render() {
         const { col } = this.props;
+        const { click } = this.state;
 
         const news = [
             {title: 'Title 1', description: 'Description', details: 'Details', image: '//materializecss.com/images/office.jpg', link: '#teste'},
@@ -22,9 +35,10 @@ class Card extends React.Component {
 
         return(
             <div className="row">
+                <p>Cliques: {click}</p>
                 {news.map((item, index) => (
                     <div key={index} className={`col s12 m${col}`}>
-                        <CardInfo data={item} />
+                        <CardInfo data={item} fnClick={this.setClick} />
                     </div>
                 ))}
             </div>

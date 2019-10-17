6c5f1523bfcde3fdea2edcc2f8b9d76521c26c9e
Rovian Vieceli <rovian.vieceli@cwi.com.br>
2019-10-17 13:11:23 -0300

    Recolocando o title que sumiu

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-import Title from './components/title/index';
+import Title from './components/header/title';
 import Navbar from './components/navbar/index';
 
 const App = (

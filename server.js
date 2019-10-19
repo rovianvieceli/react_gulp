2fb7347cbaa7494e0a6baabf46d23f004b4551c6
Rovian Vieceli <rovianvieceli@gmail.com>
2019-10-19 20:27:36 +0000

    port

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 app.use(express.static(__dirname + '/'));
 app.use('/api', require('./src/routes.jsx'));
 
-app.listen(process.env.PORT || 80, () => {
-    console.log(`Server listen on http://localhost/`);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server listen on http://localhost:3000`);
 });
